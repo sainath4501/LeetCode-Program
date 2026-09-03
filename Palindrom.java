ba5f4bc0985ae64e924ebf6b48a6775df8b63439
Sainath <sainath4501@gmail.com>
2026-09-03 21:02:40 +0530

    palindrom program

--- Palindrom.java
+++ Palindrom.java
@@ -14,7 +14,7 @@ public class Palindrom {
 
     public static void main(String[] args) {
     
-        System.out.println(palindrom(121));
+        System.out.println(palindrom(161));
         
     }
 }
