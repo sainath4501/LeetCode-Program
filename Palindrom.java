75ff3afb20181cf0930f3d76c813e8a3bc8fc1b1
Sainath <sainath4501@gmail.com>
2026-09-03 20:59:05 +0530

    add

---
+++ Palindrom.java
@@ -0,0 +1,20 @@
+public class Palindrom {
+
+    public static boolean palindrom(int x){
+        int origin=x;
+        int rev=0;
+        while (x > 0) {
+            int lastDigit=x%10;
+            rev=(rev*10)+lastDigit;
+            x=x/10;
+            
+        }
+        return origin==rev;
+    }
+
+    public static void main(String[] args) {
+    
+        System.out.println(palindrom(121));
+        
+    }
+}
