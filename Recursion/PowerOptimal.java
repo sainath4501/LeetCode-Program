3097b05f94be173280b87b3f27847489cf9621bc
Sainath <sainath4501@gmail.com>
2026-09-07 08:45:33 +0530

    add

---
+++ Recursion/PowerOptimal.java
@@ -0,0 +1,9 @@
+public class PowerOptimal {
+
+    public static double power(double x, int n){
+        return Math.pow(x,n);
+    }
+    public static void main(String args[]){
+        System.out.println(power(2, 3));
+    }
+}
